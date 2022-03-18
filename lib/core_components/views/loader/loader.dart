4356c5f4ac44a6ef4692b9e7844791dbd79e9728
nehemiekoffi <nehemiekoffi@gmail.com>
2022-03-18 16:29:30 +0000

    Enable loader size and increase loader strokeWidth

--- lib/core_components/views/loader/loader.dart
+++ lib/core_components/views/loader/loader.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:app_ui/design_tokens/colors/interface_colors.dart';
 
 class AppLoader extends StatelessWidget {
-  const AppLoader({Key? key, this.color}) : super(key: key);
+  const AppLoader({Key? key, this.color, this.size = 30}) : super(key: key);
 
   final Color? color;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      color: color ?? InterfaceColors.action.defaultColor,
-      strokeWidth: 2,
+    return SizedBox(
+      height: size,
+      width: size,
+      child: CircularProgressIndicator(
+        color: color ?? InterfaceColors.action.defaultColor,
+        strokeWidth: 2.5,
+      ),
     );
   }
 }
