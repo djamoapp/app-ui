1d4cc26936501a6f019ae0492f9a96261e60e0f0
nehemiekoffi <nehemiekoffi@gmail.com>
2022-03-23 14:05:12 +0000

    Add AppSvgIcons

---
+++ lib/design_tokens/iconography/app_svg_icons.dart
@@ -0,0 +1,12 @@
+import 'package:app_ui/core/constants/constants.dart';
+
+class AppSvgIcons {
+  AppSvgIcons._();
+
+  static const pennies = "$kAssetPrefix/assets/icons/svg/pennies.svg";
+  static const star = "$kAssetPrefix/assets/icons/svg/star.svg";
+  static const notificationFilled =
+      "$kAssetPrefix/assets/icons/svg/NotificationFilled.svg";
+  static const textMessageFilled =
+      "$kAssetPrefix/assets/icons/svg/textMessageFilled.svg";
+}
