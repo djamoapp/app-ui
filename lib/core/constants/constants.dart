5667955487dc4f7d61c97217b8454e704b81417f
Aziz Soulé <51265402+azizsoule@users.noreply.github.com>
2022-03-23 15:01:40 +0000

    addition of amount text field

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,4 +3,5 @@ const String kIconsFontFamily = "App Icons";
 const String kDjamoCardFontFamily = "OCRAStd";
 const String kPackageName = "app_ui";
 const String kAssetPrefix = "packages/$kPackageName";
+const String kDeviseSymbol = "F CFA";
 const double defaultHorisontalMargin = 16;
