19c3665983b190ac4312c1ba8f0dff983aaa29e6
nehemiekoffi <nehemiekoffi@gmail.com>
2022-03-24 20:05:26 +0000

    Fixing AppSvgIcons

--- lib/design_tokens/iconography/app_svg_icons.dart
+++ lib/design_tokens/iconography/app_svg_icons.dart
@@ -3,10 +3,10 @@ import 'package:app_ui/core/constants/constants.dart';
 class AppSvgIcons {
   AppSvgIcons._();
 
-  static const pennies = "$kAssetPrefix/assets/icons/svg/pennies.svg";
-  static const star = "$kAssetPrefix/assets/icons/svg/star.svg";
-  static const notificationFilled =
-      "$kAssetPrefix/assets/icons/svg/NotificationFilled.svg";
-  static const textMessageFilled =
+  static String get pennies => "$kAssetPrefix/assets/icons/svg/pennies.svg";
+  static String get star => "$kAssetPrefix/assets/icons/svg/star.svg";
+  static String get notificationFilled =>
+      "$kAssetPrefix/assets/icons/svg/notificationFilled.svg.svg";
+  static String get textMessageFilled =>
       "$kAssetPrefix/assets/icons/svg/textMessageFilled.svg";
 }
