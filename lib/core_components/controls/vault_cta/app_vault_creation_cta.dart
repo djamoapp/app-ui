936d1dbeca3b2c3d8eae53479c233058df1f8444
nehemiekoffi <nehemiekoffi@gmail.com>
2022-05-06 16:28:26 +0000

    Apply new UI for AppVaultCreationCTA

--- lib/core_components/controls/vault_cta/app_vault_creation_cta.dart
+++ lib/core_components/controls/vault_cta/app_vault_creation_cta.dart
@@ -1,5 +1,8 @@
+import 'package:app_ui/core/models/interface_color.dart';
 import 'package:app_ui/core_components/views/container/app_container.dart';
 import 'package:app_ui/design_tokens/colors/gradients.dart';
+import 'package:app_ui/design_tokens/colors/interface_colors.dart';
+import 'package:app_ui/design_tokens/colors/neutral_colors.dart';
 import 'package:app_ui/design_tokens/layout_and_spacing/app_gaps.dart';
 import 'package:app_ui/design_tokens/typography/typography.dart';
 import 'package:flutter/cupertino.dart';
@@ -28,10 +31,11 @@ class AppVaultCreationCTA extends StatelessWidget {
       onTap: onTap,
       child: AppContainer(
         boxDecoration: BoxDecoration(
-          gradient: AppGradients.style1,
+          color: InterfaceColors.action.specialColor,
           borderRadius: BorderRadius.circular(16),
         ),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             leading,
             AppGaps.m,
@@ -41,25 +45,34 @@ class AppVaultCreationCTA extends StatelessWidget {
                 children: [
                   Text(
                     title,
-                    style: AppTypography.label!.bMedium200!.copyWith(
-                      color: Colors.white,
+                    style: AppTypography.label!.bSmall!.copyWith(
+                      color: InterfaceColors.action.defaultColor,
                     ),
                   ),
                   AppGaps.xs,
                   Text(
                     subTitle,
-                    style: AppTypography.body!.bSmall!.copyWith(
-                      color: Colors.white,
+                    style: AppTypography.body!.small!.copyWith(
+                      color: NeutralColors.neutral700,
                     ),
                   ),
                 ],
               ),
             ),
             AppGaps.m,
-            Icon(
-              CupertinoIcons.chevron_right,
-              color: Colors.white,
-              size: 15,
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(4),
+              child: Center(
+                child: Icon(
+                  CupertinoIcons.chevron_right,
+                  color: InterfaceColors.action.defaultColor,
+                  size: 12,
+                ),
+              ),
             ),
           ],
         ),
