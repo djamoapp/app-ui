6b3377253ceb42d4ee1f1c552e5d21136d14e2dd
Aziz Soulé <51265402+azizsoule@users.noreply.github.com>
2022-06-07 15:18:12 +0000

    Update assets

---
+++ lib/design_tokens/iconography/assets.gen.dart
@@ -0,0 +1,452 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsGifGen {
+  const $AssetsGifGen();
+
+  /// File path: assets/gif/lock.gif
+  AssetGenImage get lock => const AssetGenImage('assets/gif/lock.gif');
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  $AssetsIconsColoredGen get colored => const $AssetsIconsColoredGen();
+  $AssetsIconsSvgGen get svg => const $AssetsIconsSvgGen();
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/app_icon.png
+  AssetGenImage get appIcon =>
+      const AssetGenImage('assets/images/app_icon.png');
+
+  /// File path: assets/images/djamo-logo-black.png
+  AssetGenImage get djamoLogoBlack =>
+      const AssetGenImage('assets/images/djamo-logo-black.png');
+
+  /// File path: assets/images/djamo_card_empty.png
+  AssetGenImage get djamoCardEmpty =>
+      const AssetGenImage('assets/images/djamo_card_empty.png');
+
+  /// File path: assets/images/djamo_card_empty_lock.png
+  AssetGenImage get djamoCardEmptyLock =>
+      const AssetGenImage('assets/images/djamo_card_empty_lock.png');
+}
+
+class $AssetsIconsColoredGen {
+  const $AssetsIconsColoredGen();
+
+  /// File path: assets/icons/colored/ATM.svg
+  SvgGenImage get atm => const SvgGenImage('assets/icons/colored/ATM.svg');
+
+  /// File path: assets/icons/colored/Bazar Box.svg
+  SvgGenImage get bazarBox =>
+      const SvgGenImage('assets/icons/colored/Bazar Box.svg');
+
+  /// File path: assets/icons/colored/Book.svg
+  SvgGenImage get book => const SvgGenImage('assets/icons/colored/Book.svg');
+
+  /// File path: assets/icons/colored/Box.svg
+  SvgGenImage get box => const SvgGenImage('assets/icons/colored/Box.svg');
+
+  /// File path: assets/icons/colored/Car.svg
+  SvgGenImage get car => const SvgGenImage('assets/icons/colored/Car.svg');
+
+  /// File path: assets/icons/colored/Chest - Desaturated.svg
+  SvgGenImage get chestDesaturated =>
+      const SvgGenImage('assets/icons/colored/Chest - Desaturated.svg');
+
+  /// File path: assets/icons/colored/Chest.svg
+  SvgGenImage get chest => const SvgGenImage('assets/icons/colored/Chest.svg');
+
+  /// File path: assets/icons/colored/Confettis.svg
+  SvgGenImage get confettis =>
+      const SvgGenImage('assets/icons/colored/Confettis.svg');
+
+  /// File path: assets/icons/colored/Desktop.svg
+  SvgGenImage get desktop =>
+      const SvgGenImage('assets/icons/colored/Desktop.svg');
+
+  /// File path: assets/icons/colored/Expense Limit.svg
+  SvgGenImage get expenseLimit =>
+      const SvgGenImage('assets/icons/colored/Expense Limit.svg');
+
+  /// File path: assets/icons/colored/Gift Group.svg
+  SvgGenImage get giftGroup =>
+      const SvgGenImage('assets/icons/colored/Gift Group.svg');
+
+  /// File path: assets/icons/colored/Gift.svg
+  SvgGenImage get gift => const SvgGenImage('assets/icons/colored/Gift.svg');
+
+  /// File path: assets/icons/colored/Guitar2.svg
+  SvgGenImage get guitar2 =>
+      const SvgGenImage('assets/icons/colored/Guitar2.svg');
+
+  /// File path: assets/icons/colored/POS.svg
+  SvgGenImage get pos => const SvgGenImage('assets/icons/colored/POS.svg');
+
+  /// File path: assets/icons/colored/Personal Bank.svg
+  SvgGenImage get personalBank =>
+      const SvgGenImage('assets/icons/colored/Personal Bank.svg');
+
+  /// File path: assets/icons/colored/Reloading Issue.svg
+  SvgGenImage get reloadingIssue =>
+      const SvgGenImage('assets/icons/colored/Reloading Issue.svg');
+
+  /// File path: assets/icons/colored/School Stuff 1.svg
+  SvgGenImage get schoolStuff1 =>
+      const SvgGenImage('assets/icons/colored/School Stuff 1.svg');
+
+  /// File path: assets/icons/colored/School Stuff 2.svg
+  SvgGenImage get schoolStuff2 =>
+      const SvgGenImage('assets/icons/colored/School Stuff 2.svg');
+
+  /// File path: assets/icons/colored/Suitcase.svg
+  SvgGenImage get suitcase =>
+      const SvgGenImage('assets/icons/colored/Suitcase.svg');
+
+  /// File path: assets/icons/colored/Suitcases.svg
+  SvgGenImage get suitcases =>
+      const SvgGenImage('assets/icons/colored/Suitcases.svg');
+
+  /// File path: assets/icons/colored/Transfer Issue.svg
+  SvgGenImage get transferIssue =>
+      const SvgGenImage('assets/icons/colored/Transfer Issue.svg');
+
+  /// File path: assets/icons/colored/Vault Cash In - Colorful.svg
+  SvgGenImage get vaultCashInColorful =>
+      const SvgGenImage('assets/icons/colored/Vault Cash In - Colorful.svg');
+
+  /// File path: assets/icons/colored/Vault Cash In.svg
+  SvgGenImage get vaultCashIn =>
+      const SvgGenImage('assets/icons/colored/Vault Cash In.svg');
+
+  /// File path: assets/icons/colored/Vault.svg
+  SvgGenImage get vault => const SvgGenImage('assets/icons/colored/Vault.svg');
+}
+
+class $AssetsIconsSvgGen {
+  const $AssetsIconsSvgGen();
+
+  /// File path: assets/icons/svg/account.svg
+  SvgGenImage get account => const SvgGenImage('assets/icons/svg/account.svg');
+
+  /// File path: assets/icons/svg/accountFilled.svg
+  SvgGenImage get accountFilled =>
+      const SvgGenImage('assets/icons/svg/accountFilled.svg');
+
+  /// File path: assets/icons/svg/add.svg
+  SvgGenImage get add => const SvgGenImage('assets/icons/svg/add.svg');
+
+  /// File path: assets/icons/svg/addCircularGraph.svg
+  SvgGenImage get addCircularGraph =>
+      const SvgGenImage('assets/icons/svg/addCircularGraph.svg');
+
+  /// File path: assets/icons/svg/addMoney.svg
+  SvgGenImage get addMoney =>
+      const SvgGenImage('assets/icons/svg/addMoney.svg');
+
+  /// File path: assets/icons/svg/alert.svg
+  SvgGenImage get alert => const SvgGenImage('assets/icons/svg/alert.svg');
+
+  /// File path: assets/icons/svg/arrowDownLeft.svg
+  SvgGenImage get arrowDownLeft =>
+      const SvgGenImage('assets/icons/svg/arrowDownLeft.svg');
+
+  /// File path: assets/icons/svg/arrowDownRight.svg
+  SvgGenImage get arrowDownRight =>
+      const SvgGenImage('assets/icons/svg/arrowDownRight.svg');
+
+  /// File path: assets/icons/svg/arrowTopRight.svg
+  SvgGenImage get arrowTopRight =>
+      const SvgGenImage('assets/icons/svg/arrowTopRight.svg');
+
+  /// File path: assets/icons/svg/bank.svg
+  SvgGenImage get bank => const SvgGenImage('assets/icons/svg/bank.svg');
+
+  /// File path: assets/icons/svg/bonus.svg
+  SvgGenImage get bonus => const SvgGenImage('assets/icons/svg/bonus.svg');
+
+  /// File path: assets/icons/svg/box.svg
+  SvgGenImage get box => const SvgGenImage('assets/icons/svg/box.svg');
+
+  /// File path: assets/icons/svg/cadena.svg
+  SvgGenImage get cadena => const SvgGenImage('assets/icons/svg/cadena.svg');
+
+  /// File path: assets/icons/svg/calendar.svg
+  SvgGenImage get calendar =>
+      const SvgGenImage('assets/icons/svg/calendar.svg');
+
+  /// File path: assets/icons/svg/calendarRefresh.svg
+  SvgGenImage get calendarRefresh =>
+      const SvgGenImage('assets/icons/svg/calendarRefresh.svg');
+
+  /// File path: assets/icons/svg/card.svg
+  SvgGenImage get card => const SvgGenImage('assets/icons/svg/card.svg');
+
+  /// File path: assets/icons/svg/cardFilled.svg
+  SvgGenImage get cardFilled =>
+      const SvgGenImage('assets/icons/svg/cardFilled.svg');
+
+  /// File path: assets/icons/svg/check.svg
+  SvgGenImage get check => const SvgGenImage('assets/icons/svg/check.svg');
+
+  /// File path: assets/icons/svg/clickingIndex.svg
+  SvgGenImage get clickingIndex =>
+      const SvgGenImage('assets/icons/svg/clickingIndex.svg');
+
+  /// File path: assets/icons/svg/copy.svg
+  SvgGenImage get copy => const SvgGenImage('assets/icons/svg/copy.svg');
+
+  /// File path: assets/icons/svg/cross.svg
+  SvgGenImage get cross => const SvgGenImage('assets/icons/svg/cross.svg');
+
+  /// File path: assets/icons/svg/crown.svg
+  SvgGenImage get crown => const SvgGenImage('assets/icons/svg/crown.svg');
+
+  /// File path: assets/icons/svg/desktopWebBrowser.svg
+  SvgGenImage get desktopWebBrowser =>
+      const SvgGenImage('assets/icons/svg/desktopWebBrowser.svg');
+
+  /// File path: assets/icons/svg/djamo.svg
+  SvgGenImage get djamo => const SvgGenImage('assets/icons/svg/djamo.svg');
+
+  /// File path: assets/icons/svg/download.svg
+  SvgGenImage get download =>
+      const SvgGenImage('assets/icons/svg/download.svg');
+
+  /// File path: assets/icons/svg/eye.svg
+  SvgGenImage get eye => const SvgGenImage('assets/icons/svg/eye.svg');
+
+  /// File path: assets/icons/svg/fingerprint.svg
+  SvgGenImage get fingerprint =>
+      const SvgGenImage('assets/icons/svg/fingerprint.svg');
+
+  /// File path: assets/icons/svg/gear.svg
+  SvgGenImage get gear => const SvgGenImage('assets/icons/svg/gear.svg');
+
+  /// File path: assets/icons/svg/info.svg
+  SvgGenImage get info => const SvgGenImage('assets/icons/svg/info.svg');
+
+  /// File path: assets/icons/svg/money.svg
+  SvgGenImage get money => const SvgGenImage('assets/icons/svg/money.svg');
+
+  /// File path: assets/icons/svg/noteText.svg
+  SvgGenImage get noteText =>
+      const SvgGenImage('assets/icons/svg/noteText.svg');
+
+  /// File path: assets/icons/svg/notification.svg
+  SvgGenImage get notification =>
+      const SvgGenImage('assets/icons/svg/notification.svg');
+
+  /// File path: assets/icons/svg/notificationFilled.svg
+  SvgGenImage get notificationFilled =>
+      const SvgGenImage('assets/icons/svg/notificationFilled.svg');
+
+  /// File path: assets/icons/svg/numpad.svg
+  SvgGenImage get numpad => const SvgGenImage('assets/icons/svg/numpad.svg');
+
+  /// File path: assets/icons/svg/password.svg
+  SvgGenImage get password =>
+      const SvgGenImage('assets/icons/svg/password.svg');
+
+  /// File path: assets/icons/svg/pencil.svg
+  SvgGenImage get pencil => const SvgGenImage('assets/icons/svg/pencil.svg');
+
+  /// File path: assets/icons/svg/pennies.svg
+  SvgGenImage get pennies => const SvgGenImage('assets/icons/svg/pennies.svg');
+
+  /// File path: assets/icons/svg/plus.svg
+  SvgGenImage get plus => const SvgGenImage('assets/icons/svg/plus.svg');
+
+  /// File path: assets/icons/svg/profile.svg
+  SvgGenImage get profile => const SvgGenImage('assets/icons/svg/profile.svg');
+
+  /// File path: assets/icons/svg/reccurentWire.svg
+  SvgGenImage get reccurentWire =>
+      const SvgGenImage('assets/icons/svg/reccurentWire.svg');
+
+  /// File path: assets/icons/svg/replaceCard.svg
+  SvgGenImage get replaceCard =>
+      const SvgGenImage('assets/icons/svg/replaceCard.svg');
+
+  /// File path: assets/icons/svg/resetPassword.svg
+  SvgGenImage get resetPassword =>
+      const SvgGenImage('assets/icons/svg/resetPassword.svg');
+
+  /// File path: assets/icons/svg/smartphone.svg
+  SvgGenImage get smartphone =>
+      const SvgGenImage('assets/icons/svg/smartphone.svg');
+
+  /// File path: assets/icons/svg/stand.svg
+  SvgGenImage get stand => const SvgGenImage('assets/icons/svg/stand.svg');
+
+  /// File path: assets/icons/svg/star.svg
+  SvgGenImage get star => const SvgGenImage('assets/icons/svg/star.svg');
+
+  /// File path: assets/icons/svg/starFilled.svg
+  SvgGenImage get starFilled =>
+      const SvgGenImage('assets/icons/svg/starFilled.svg');
+
+  /// File path: assets/icons/svg/stats.svg
+  SvgGenImage get stats => const SvgGenImage('assets/icons/svg/stats.svg');
+
+  /// File path: assets/icons/svg/statsFilled.svg
+  SvgGenImage get statsFilled =>
+      const SvgGenImage('assets/icons/svg/statsFilled.svg');
+
+  /// File path: assets/icons/svg/support.svg
+  SvgGenImage get support => const SvgGenImage('assets/icons/svg/support.svg');
+
+  /// File path: assets/icons/svg/supportFilled.svg
+  SvgGenImage get supportFilled =>
+      const SvgGenImage('assets/icons/svg/supportFilled.svg');
+
+  /// File path: assets/icons/svg/textMessage.svg
+  SvgGenImage get textMessage =>
+      const SvgGenImage('assets/icons/svg/textMessage.svg');
+
+  /// File path: assets/icons/svg/textMessageFilled.svg
+  SvgGenImage get textMessageFilled =>
+      const SvgGenImage('assets/icons/svg/textMessageFilled.svg');
+
+  /// File path: assets/icons/svg/trash.svg
+  SvgGenImage get trash => const SvgGenImage('assets/icons/svg/trash.svg');
+
+  /// File path: assets/icons/svg/vault.svg
+  SvgGenImage get vault => const SvgGenImage('assets/icons/svg/vault.svg');
+
+  /// File path: assets/icons/svg/vaultFilled.svg
+  SvgGenImage get vaultFilled =>
+      const SvgGenImage('assets/icons/svg/vaultFilled.svg');
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsGifGen gif = $AssetsGifGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale = 1.0,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  String get path => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    Color? color,
+    BlendMode colorBlendMode = BlendMode.srcIn,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    Clip clipBehavior = Clip.hardEdge,
+    bool cacheColorFilter = false,
+    SvgTheme? theme,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+      theme: theme,
+    );
+  }
+
+  String get path => _assetName;
+}
