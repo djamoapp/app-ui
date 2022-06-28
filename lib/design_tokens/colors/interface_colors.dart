742b49d6d5b57ac1a0d3670eefeb5e1111a7149c
Aziz Soulé <51265402+azizsoule@users.noreply.github.com>
2022-06-28 17:54:04 +0000

    Format colors files

--- lib/design_tokens/colors/interface_colors.dart
+++ lib/design_tokens/colors/interface_colors.dart
@@ -5,23 +5,27 @@ class InterfaceColors {
   InterfaceColors._();
 
   static final InterfaceColor action = InterfaceColor(
-      defaultColor: const Color(0xff0C29FF),
-      disabledColor: const Color(0xffC2C9FF),
-      backGroundColor: const Color(0xffF6F7FB),
-      specialColor: const Color(0xffE0E4FF));
+    defaultColor: const Color(0xff0C29FF),
+    disabledColor: const Color(0xffC2C9FF),
+    backGroundColor: const Color(0xffF6F7FB),
+    specialColor: const Color(0xffE0E4FF),
+  );
 
   static final InterfaceColor success = InterfaceColor(
-      defaultColor: const Color(0xff31B67A),
-      disabledColor: const Color(0xffB4E0CC),
-      backGroundColor: const Color(0xffEBFAF3));
+    defaultColor: const Color(0xff31B67A),
+    disabledColor: const Color(0xffB4E0CC),
+    backGroundColor: const Color(0xffEBFAF3),
+  );
 
   static final InterfaceColor alert = InterfaceColor(
-      defaultColor: const Color(0xffDDC25A),
-      disabledColor: const Color(0xffFFEFC2),
-      backGroundColor: const Color(0xffFFFAEB));
+    defaultColor: const Color(0xffDDC25A),
+    disabledColor: const Color(0xffFFEFC2),
+    backGroundColor: const Color(0xffFFFAEB),
+  );
 
   static final InterfaceColor error = InterfaceColor(
-      defaultColor: const Color(0xffFD3C2D),
-      disabledColor: const Color(0xffFAC2BE),
-      backGroundColor: const Color(0xffFFECEB));
+    defaultColor: const Color(0xffFD3C2D),
+    disabledColor: const Color(0xffFAC2BE),
+    backGroundColor: const Color(0xffFFECEB),
+  );
 }
